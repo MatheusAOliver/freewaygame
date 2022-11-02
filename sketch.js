50c352db68ef90f589d774e6a58a3cc26328d732
MatheusAOliver <116984835+MatheusAOliver@users.noreply.github.com>
2022-11-02 14:03:16 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+sketch.js
+function setup(){
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw(){
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+ 
